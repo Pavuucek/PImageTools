73b1b7d96ce268b580fd80e1024c069978b49b9d
Michal Kuncl <michal.kuncl@gmail.com>
2016-01-28 10:16:41 +0100

    initial import

---
+++ Shrinker/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PImageTools.Shrinker
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
