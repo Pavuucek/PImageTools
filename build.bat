6f0fdcde61701a48a547d8a9c8dd65a6ee02ed23
Michal Kuncl <michal.kuncl@gmail.com>
2016-01-28 10:11:22 +0100

    add GitVersioner.exe

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 rem call version.bat
-%windir%\Microsoft.Net\Framework\v4.0.30319\msbuild ArachNGIN.sln /m /property:Configuration=Debug /property:Platform="Any CPU"
-%windir%\Microsoft.Net\Framework\v4.0.30319\msbuild ArachNGIN.sln /m /property:Configuration=Release /property:Platform="Any CPU"
+%windir%\Microsoft.Net\Framework\v4.0.30319\msbuild PImageTools.sln /m /property:Configuration=Debug /property:Platform="Any CPU"
+%windir%\Microsoft.Net\Framework\v4.0.30319\msbuild PImageTools.sln /m /property:Configuration=Release /property:Platform="Any CPU"
 
 if ERRORLEVEL 1 pause
 
