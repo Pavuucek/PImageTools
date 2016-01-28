b94349c687681e2f46bf512b996d2d4c3d5fdb04
Michal Kuncl <michal.kuncl@gmail.com>
2016-01-28 12:01:07 +0100

    update GitVersioner.exe

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 rem call version.bat
 %windir%\Microsoft.Net\Framework\v4.0.30319\msbuild PImageTools.sln /m /property:Configuration=Debug /property:Platform="Any CPU"
 %windir%\Microsoft.Net\Framework\v4.0.30319\msbuild PImageTools.sln /m /property:Configuration=Release /property:Platform="Any CPU"
-
+gitversioner.exe ba
 if ERRORLEVEL 1 pause
 
 gitversioner.exe w zip.bat
